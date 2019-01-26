12c8fda608f7b7e16e5861f6deb963e6da8e2e1c
evoosa <eevvoossaa@gmail.com>
2019-01-26 22:10:56 +0200

    added convert hex to rgb, consoles it on screen

--- color_math.js
+++ color_math.js
@@ -6,6 +6,21 @@ var max_color_diversity = 10;
 var max_cmy_val = 100;
 
 
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
 function rgb_to_cmy_single_value(value) {
     return max_cmy_val - value / 2.55
 }
@@ -31,5 +46,8 @@ function round_cmy_triplet(cmy_triplet) {
 }
 
 function rgb_to_rounded_cmy(rgb_triplet) {
+    console.log(round_cmy_triplet(rgb_to_cmy(rgb_triplet)));
     return round_cmy_triplet(rgb_to_cmy(rgb_triplet))
 }
+
+//function cmy_to_rgb
